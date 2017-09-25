fd1ec866e424b4f8fec2ac5965fa84e16367676e
Cris <c.caratti@me.com>
2017-09-25 09:43:21 +0100

    initial commit

---
+++ conversion.js
@@ -0,0 +1,47 @@
+///////////////////////////////////////////////////////////////////////////////////////////
+//////////////////////////////// Fahranheit to Celsius ////////////////////////////////////
+///////////////////////////////////////////////////////////////////////////////////////////
+
+
+document.getElementById('button').addEventListener("click", function() {
+ 
+ //input F value
+ var f = document.getElementById('input1').value;
+ 
+ // exe conversion
+ function C(f) {
+   return 5/9*(f - 32);
+  } 
+ //console.log(C(572));
+ 
+// print result
+ document.getElementById('result').textContent = Math.round(C(f));
+ document.getElementById('calculation').textContent = C;
+  
+});
+
+
+
+
+///////////////////////////////////////////////////////////////////////////////////////////
+//////////////////////////////// Celsius to Fahranheit ////////////////////////////////////
+///////////////////////////////////////////////////////////////////////////////////////////
+
+
+
+
+document.getElementById('button2').addEventListener("click", function() {
+ 
+ //input F value
+ var celsius = document.getElementById('input2').value;
+ 
+ // exe conversion
+ function F(celsius) {
+   return 9/5*celsius + 32;
+  } 
+ 
+// print result
+ document.getElementById('result').textContent = Math.round(F(celsius));
+ document.getElementById('calculation').textContent = F;
+  
+});
